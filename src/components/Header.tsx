206383b0795fd3c0a73136b759df117f666659cb
Roger Vicini <rogerluisvicini1@gmail.com>
2026-08-03 22:44:13 -0300

    Mundo 2: primeiro componente Header e ajuste de paths

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Header() {
+    return (
+        <View>
+            <Text>Bem-vindo ao ChefUP</Text>
+        </View>
+    );
+}
